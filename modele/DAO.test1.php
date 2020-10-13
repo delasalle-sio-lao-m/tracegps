d02fbbb330920ec18040e2304c5e6f8cc7b7c624
delasalle-sio-jouan-t <delasalle.sio.jouan.t@gmail.com>
2020-10-13 17:22:25 +0200

    oui

--- modele/DAO.test1.php
+++ modele/DAO.test1.php
@@ -55,17 +55,6 @@ echo ('<br>');
 }
 */
 
-// test de la méthode getLesPointsDeTrace ---------------------------------------------------------
-// modifié par Jim le 13/8/2018
-echo "<h3>Test de getLesPointsDeTrace : </h3>";
-$lesPoints = $dao->getLesPointsDeTrace(1);
-$nbPoints = sizeof($lesPoints);
-echo "<p>Nombre de points de la trace 1 : " . $nbPoints . "</p>";
-// affichage des points
-foreach ($lesPoints as $unPoint)
-{   echo ($unPoint->toString());
-echo ('<br>');
-}
 
 
 
