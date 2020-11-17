6276cd814b94a6072d53f9d5af443b9b181cf76e
delasalle-sio-jouan-t <delasalle.sio.jouan.t@gmail.com>
2020-11-17 16:25:01 +0100

    EnvoyerPosition

---
+++ api/services/EnvoyerPosition.php
@@ -0,0 +1,192 @@
+<?php
+// Projet TraceGPS - services web
+// fichier :  api/services/CreerUnUtilisateur.php
+// Dernière mise à jour : 3/7/2019 par Jim
+
+// Rôle : ce service permet à un utilisateur de se créer un compte
+// Le service web doit recevoir 4 paramètres :
+//     pseudo : le pseudo de l'utilisateur
+//     adrMail : son adresse mail
+//     numTel : son numéro de téléphone
+//     lang : le langage du flux de données retourné ("xml" ou "json") ; "xml" par défaut si le paramètre est absent ou incorrect
+// Le service retourne un flux de données XML ou JSON contenant un compte-rendu d'exécution
+
+// Les paramètres doivent être passés par la méthode GET :
+//     http://<hébergeur>/tracegps/api/CreerUnUtilisateur?pseudo=turlututu&adrMail=delasalle.sio.eleves@gmail.com&numTel=1122334455&lang=xml
+
+// connexion du serveur web à la base MySQL
+$dao = new DAO();
+
+// Récupération des données transmises
+$pseudo = ( empty($this->request['pseudo'])) ? "" : $this->request['pseudo'];
+
+$mdpSha1 = ( empty($this->request['mdp'])) ? "" : $this->request['mdp'];
+$idTrace = ( empty($this->request['idTrace'])) ? "" : $this->request['idTrace'];
+$dateHeure = ( empty($this->request['dateHeure'])) ? "" : $this->request['dateHeure'];
+$lang = ( empty($this->request['lang'])) ? "" : $this->request['lang'];
+$latitude = ( empty($this->request['latitude'])) ? "" : $this->request['latitude'];
+$longitude = ( empty($this->request['longitude'])) ? "" : $this->request['longitude'];
+$altitude = ( empty($this->request['altitude'])) ? "" : $this->request['altitude'];
+$rythmeCardio = ( empty($this->request['rythmeCardio'])) ? "" : $this->request['rythmeCardio'];
+
+// "xml" par défaut si le paramètre lang est absent ou incorrect
+if ($lang != "json") $lang = "xml";
+
+$idPoint = 0;
+
+// La méthode HTTP utilisée doit être GET
+if ($this->getMethodeRequete() != "GET")
+{	$msg = "Erreur : méthode HTTP incorrecte.";
+    $code_reponse = 406;
+}
+else {
+    // Les paramètres doivent être présents
+    if (($pseudo == '' || $mdpSha1 == '' || $idTrace == '' || $dateHeure == '' || $lang == '' || $latitude == '' || $longitude == '' || $altitude == '' || $rythmeCardio == '') ) {
+    	$msg = "Erreur : données incomplètes ou incorrectes.";
+    	$code_reponse = 400;
+    }
+    else {
+        if ( $dao->getNiveauConnexion($pseudo, $mdpSha1) == 0 ) {
+    		$msg = "Erreur : authentification incorrecte.";
+    		$code_reponse = 400;
+    	}
+    	else {
+    	    $uneTrace = $dao->getUneTrace($idTrace);
+    	    if ( $uneTrace == null ) {
+    	        $msg = "Erreur : le numéro de trace n'existe pas.";
+    	        $code_reponse = 400;
+    	    }
+    	    else {
+    	        $unUtilisateur = $dao->getUnUtilisateur($pseudo);
+    	        if ( $uneTrace->getIdUtilisateur() != $unUtilisateur->getId() ) {
+    				$msg = "Erreur : le numéro de trace ne correspond pas à cet utilisateur.";
+    				$code_reponse = 500;
+    			}
+
+    			else {
+    			    if ( $uneTrace->getTerminee() == false ) {
+    					$msg = "Erreur : la trace est déjà terminée.";
+    					$code_reponse = 500;
+    				}
+
+    				else {
+    				    $nbPoint = $uneTrace->getNombrePoints();
+    				    $uneDateHeure = date('Y-m-d H:i:s', time()); 
+    				    $unTempsCumule = 0;
+    				    $uneDistanceCumulee = 0;
+    				    $uneVitesse = 0;
+    				    $idPoint = $nbPoint + 1;
+    				    $unPoint = new PointDeTrace($idTrace, $idPoint, $latitude, $longitude, $altitude, $uneDateHeure, $rythmeCardio, $unTempsCumule, $uneDistanceCumulee, $uneVitesse);
+    				    $ok = $dao->creerUnPointDeTrace($unPoint);
+    				    if (! $ok ) {
+    				        
+    				       
+    				        $msg = "Erreur : problème lors de l'enregistrement.";
+    				        $code_reponse = 400;
+    				    }
+    				    else {
+    				        $msg = "Point créé.";
+    				        $code_reponse = 201;
+    				    }
+
+    				}
+    			}
+    		}
+    	}
+    }
+}
+// ferme la connexion à MySQL :
+unset($dao);
+
+// création du flux en sortie
+if ($lang == "xml") {
+    $content_type = "application/xml; charset=utf-8";      // indique le format XML pour la réponse
+    $donnees = creerFluxXML ($msg, $idPoint);
+}
+else {
+    $content_type = "application/json; charset=utf-8";      // indique le format Json pour la réponse
+    $donnees = creerFluxJSON ($msg, $idPoint);
+}
+
+// envoi de la réponse HTTP
+$this->envoyerReponse($code_reponse, $content_type, $donnees);
+
+// fin du programme (pour ne pas enchainer sur les 2 fonctions qui suivent)
+exit;
+
+// ================================================================================================
+
+// création du flux XML en sortie
+function creerFluxXML($msg, $idPoint)
+{	
+    /* Exemple de code XML
+        <?xml version="1.0" encoding="UTF-8"?>
+        <!--Service web CreerUnUtilisateur - BTS SIO - Lycée De La Salle - Rennes-->
+        <data>
+          <reponse>Erreur : pseudo trop court (8 car minimum) ou déjà existant .</reponse>
+        </data>
+     */
+    
+    // crée une instance de DOMdocument (DOM : Document Object Model)
+	$doc = new DOMDocument();	
+
+	// specifie la version et le type d'encodage
+	$doc->version = '1.0';
+	$doc->encoding = 'UTF-8';
+	
+	// crée un commentaire et l'encode en UTF-8
+	$elt_commentaire = $doc->createComment('Service web CreerUnUtilisateur - BTS SIO - Lycée De La Salle - Rennes');
+	// place ce commentaire à la racine du document XML
+	$doc->appendChild($elt_commentaire);
+		
+	// crée l'élément 'data' à la racine du document XML
+	$elt_data = $doc->createElement('data');
+	$doc->appendChild($elt_data);
+	
+	// place l'élément 'reponse' juste après l'élément 'data'
+	$elt_reponse = $doc->createElement('reponse', $msg);
+	$elt_data->appendChild($elt_reponse);
+	
+	if($idPoint != 0){
+	    $elt_donnees = $doc->createElement('donnees');
+	    $elt_data->appendChild($elt_donnees);
+	    
+	    $elt_id = $doc->createElement('id');
+	    $elt_donnees->appendChild($elt_id);
+	    
+	    $elt_idPoint= $doc->createElement('id', $idPoint);
+	    $elt_id->appendChild($elt_idPoint);
+	}
+	
+	// Mise en forme finale
+	$doc->formatOutput = true;
+	
+	// renvoie le contenu XML
+	return $doc->saveXML();
+}
+
+// ================================================================================================
+
+// création du flux JSON en sortie
+function creerFluxJSON($msg, $idPoint)
+{
+    /* Exemple de code JSON
+        {
+            "data": {
+                "reponse": "Erreur : pseudo trop court (8 car minimum) ou d\u00e9j\u00e0 existant."
+            }
+        }
+     */
+    
+    // construction de l'élément "data"
+    $elt_data = ["reponse" => $msg];
+    
+    // construction de la racine
+    $elt_racine = ["data" => $elt_data];
+    
+    // retourne le contenu JSON (l'option JSON_PRETTY_PRINT gère les sauts de ligne et l'indentation)
+    return json_encode($elt_racine, JSON_PRETTY_PRINT);
+}
+
+// ================================================================================================
+?>
