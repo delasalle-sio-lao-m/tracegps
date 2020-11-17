83c042403a0b59a0de1eb1285fb083c302c3bcca
AlanCormier <delasalle.sio.cormier.a@gmail.com>
2020-11-17 15:24:31 +0100

    Ajout service Web DemanderMdp.php

--- api/services/DemanderMdp.php
+++ api/services/DemanderMdp.php
@@ -1,19 +1,7 @@
 <?php
 // Projet TraceGPS - services web
-// fichier :  api/services/ChangerDeMdp.php
-// Dernière mise à jour : 3/7/2019 par Jim
-
-// Rôle : ce service permet à un utilisateur de changer son mot de passe
-// Le service web doit recevoir 5 paramètres :
-//     pseudo : le pseudo de l'utilisateur
-//     mdp : l'ancien mot de passe hashé en sha1
-//     nouveauMdp : le nouveau mot de passe
-//     confirmationMdp : la confirmation du nouveau mot de passe
-//     lang : le langage du flux de données retourné ("xml" ou "json") ; "xml" par défaut si le paramètre est absent ou incorrect
-// Le service retourne un flux de données XML ou JSON contenant un compte-rendu d'exécution
-
-// Les paramètres doivent être passés par la méthode GET :
-//     http://<hébergeur>/tracegps/api/ChangerDeMdppseudo=europa&mdp=13e3668bbee30b004380052b086457b014504b3e&nouveauMdp=123&confirmationMdp=123&lang=xml
+// fichier :  api/services/DemanderMdp.php
+// Dernière mise à jour : 17/11/2020 par Alan Cormier
 
 // connexion du serveur web à la base MySQL
 $dao = new DAO();
@@ -32,16 +20,17 @@ $code_reponse = 406;
 }
 else {
     // Les paramètres doivent être présents
-    if ( $pseudo == "" || $mdpSha1 == "" || $nouveauMdp == "" || $confirmationMdp == "" ) {
+    if ( $pseudo == "" ) {
         $msg = "Erreur : données incomplètes.";
         $code_reponse = 400;
     }
     else {
-        if ( $dao->existePseudoUtilisateur($pseudo) == false) {
+        if ( !$dao->existePseudoUtilisateur($pseudo)) {
             $msg = 'Erreur : pseudo inexistant.';
             $code_reponse = 400;
         }
         else {
+            $nouveauMdp = Outils::creerMdp();
             // enregistre le nouveau mot de passe de l'utilisateur dans la bdd après l'avoir codé en sha1
             $ok = $dao->modifierMdpUtilisateur ($pseudo, $nouveauMdp);
             if ( ! $ok ) {
@@ -103,7 +92,7 @@ function creerFluxXML($msg)
     $doc->encoding = 'UTF-8';
     
     // crée un commentaire et l'encode en UTF-8
-    $elt_commentaire = $doc->createComment('Service web ChangerDeMdp - BTS SIO - Lycée De La Salle - Rennes');
+    $elt_commentaire = $doc->createComment('Service web DemanderMdp - BTS SIO - Lycée De La Salle - Rennes');
     // place ce commentaire à la racine du document XML
     $doc->appendChild($elt_commentaire);
     
