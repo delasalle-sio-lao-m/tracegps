9f69ac3c759d0e5231b5ec46e30ee1c7938e6bed
delasalle-sio-lao-m <delasalle.sio.lao.m@gmail.com>
2021-03-30 13:59:09 +0200

    MAJ DemanderAutorisation + RetirerAutorisation

--- api/services/DemanderUneAutorisation.php
+++ api/services/DemanderUneAutorisation.php
@@ -37,24 +37,23 @@ $code_reponse = 406;
 else {
     // Les paramètres doivent être présents et corrects
     if ( $pseudo == "" || $mdpSha1 == "" || $pseudoDestinataire == "" || $texteMessage == "" || $nomPrenom == "" || ( $lang != "xml" && $lang != "json" ) )
-    {	$message = "Erreur : données incomplètes.";
+    {	$msg = "Erreur : données incomplètes.";
     $code_reponse = 400;
     }
     
     else
     {	// test de l'authentification de l'utilisateur
         // la méthode getNiveauConnexion de la classe DAO retourne les valeurs 0 (non identifié) ou 1 (utilisateur) ou 2 (administrateur)
-        $niveauConnexion = $dao->getNiveauConnexion($pseudo, $mdpSha1);        
-        
-        if ( $niveauConnexion == 0 )
-        {  $message = "Erreur : authentification incorrecte.";
-        $code_reponse = 401;
+         
+        if ( $dao->getNiveauConnexion($pseudo, $mdpSha1) == 0 ) {
+            $msg = "Erreur : authentification incorrecte.";
+            $code_reponse = 401;
         }
         
         else
         {	
             if (!$dao->existePseudoUtilisateur($pseudoDestinataire))
-            {	$message = "Erreur : pseudo utilisateur inexistant.";
+            {	$msg = "Erreur : pseudo utilisateur inexistant.";
             $code_reponse = 400;
             }
             else
@@ -81,11 +80,11 @@ else {
                 $contenuMail .= "http://localhost/ws-php-lao/tracegps/api/ValiderDemandeAutorisation?a=". $mdpDestinataire."&b=". $pseudoDestinataire ."&c=".$pseudo. "&d=0"."\n\n";
                 $ok = Outils::envoyerMail($adrMailDemandeur, $sujetMail, $contenuMail, $ADR_MAIL_EMETTEUR);
                 if ( ! $ok ) {
-                    $message = "Erreur : l'envoi du courriel au demandeur a rencontré un problème.";
+                    $msg = "Erreur : l'envoi du courriel au demandeur a rencontré un problème.";
                     $code_reponse = 500;
                 }
                 else {
-                    $message = "Autorisation enregistrée.<br>". $pseudoDestinataire . " va recevoir un courriel de confirmation.";
+                    $msg = "Autorisation enregistrée. ". $pseudoDestinataire . " va recevoir un courriel de confirmation.";
                     $code_reponse = 200;
                 }
                     
@@ -94,16 +93,87 @@ else {
     }
 }
 unset($dao);   // ferme la connexion à MySQL
+
+// création du flux en sortie
+if ($lang == "xml") {
+    $content_type = "application/xml; charset=utf-8";      // indique le format XML pour la réponse
+    $donnees = creerFluxXML($msg);
+}
+else {
+    $content_type = "application/json; charset=utf-8";      // indique le format Json pour la réponse
+    $donnees = creerFluxJSON($msg);
+}
+
+// envoi de la réponse HTTP
+$this->envoyerReponse($code_reponse, $content_type, $donnees);
+
+// fin du programme (pour ne pas enchainer sur les 2 fonctions qui suivent)
+exit;
+
+// ================================================================================================
+
+// création du flux XML en sortie
+function creerFluxXML($msg)
+{
+    /*
+     * Exemple de code XML
+     <?xml version="1.0" encoding="UTF-8"?>
+     <!--Service web DemanderUneAutorisation - BTS SIO - Lycée De La Salle - Rennes-->
+     <data>
+     <reponse>............. (message retourné par le service web) ...............</reponse>
+     </data>
+     */
+    
+    
+    // crée une instance de DOMdocument (DOM : Document Object Model)
+    $doc = new DOMDocument();
+    
+    // specifie la version et le type d'encodage
+    $doc->version = '1.0';
+    $doc->encoding = 'UTF-8';
+    
+    // crée un commentaire et l'encode en UTF-8
+    $elt_commentaire = $doc->createComment('Service web DemanderUneAutorisation - BTS SIO - Lycée De La Salle - Rennes');
+    // place ce commentaire à la racine du document XML
+    $doc->appendChild($elt_commentaire);
+    
+    // crée l'élément 'data' à la racine du document XML
+    $elt_data = $doc->createElement('data');
+    $doc->appendChild($elt_data);
+    
+    // place l'élément 'reponse' dans l'élément 'data'
+    $elt_reponse = $doc->createElement('reponse', $msg);
+    $elt_data->appendChild($elt_reponse);
+    
+    // Mise en forme finale
+    $doc->formatOutput = true;
+    
+    // renvoie le contenu XML
+    return $doc->saveXML();
+}
+
+// ================================================================================================
+
+// création du flux JSON en sortie
+function creerFluxJSON($msg)
+{
+    /* Exemple de code JSON
+     {
+     "data": {
+     "reponse": "Erreur : authentification incorrecte."
+     }
+     }
+     */
+    
+    // construction de l'élément "data"
+    $elt_data = ["reponse" => $msg];
+    
+    // construction de la racine
+    $elt_racine = ["data" => $elt_data];
+    
+    // retourne le contenu JSON (l'option JSON_PRETTY_PRINT gère les sauts de ligne et l'indentation)
+    return json_encode($elt_racine, JSON_PRETTY_PRINT);
+}
+
+// ================================================================================================
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>Validation TraceGPS</title>
-	<style type="text/css">body {font-family: Arial, Helvetica, sans-serif; font-size: small;}</style>
-</head>
-<body>
-	<p><?php echo $message; ?></p>
-	<p><a href="Javascript:window.close();">Fermer</a></p>
-</body>
-</html>
